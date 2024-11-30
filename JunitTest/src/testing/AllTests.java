a371f1582b60884570e0dae621a82f03a4c6d9c3
Hossain Al Masud <L00186111@atu.ie>
2024-11-30 00:12:38 +0000

    Update AllTests.java

--- JunitTest/src/testing/AllTests.java
+++ JunitTest/src/testing/AllTests.java
@@ -1,10 +1,14 @@
 package testing;
 
-import org.junit.platform.suite.api.SelectClasses;
-import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SelectClasses; // Import to specify which test classes to include in the suite
+import org.junit.platform.suite.api.Suite; // Import to define a test suite
 
+/**
+ * A test suite that aggregate multiple test classes.
+ * This suite rubs all test cases from the 'squareUnit' and 'sumUnit' test classes.
+ */
 @Suite
-@SelectClasses({ squareUnit.class, sumUnit.class })
+@SelectClasses({ squareUnit.class, sumUnit.class }) // Specifies the test classes to include in the suite
 public class AllTests {
-
+	// This class is used as a test suite entry point and does not require additional implementation
 }
